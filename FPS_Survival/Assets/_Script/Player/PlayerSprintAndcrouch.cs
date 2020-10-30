132e3d75ef93adeaf29478d8c7db0af12c9c96d2
Sunil pargi <sunilpargi121@gmail.com>
2020-10-30 20:54:33 +0530

    added player movement ,sprinting,crouching, look around

---
+++ FPS_Survival/Assets/_Script/Player/PlayerSprintAndcrouch.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndcrouch : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    public float sprint_Speed = 10f;
+    public float move_Speed = 5f;
+    public float crouch_Speed = 2f;
+
+    private Transform look_Root;
+    private float stand_Height = 1.6f;
+    private float crouch_Height = 1f;
+
+    private bool is_Crouching;
+
+
+    private void Awake()
+    {
+
+        playerMovement = GetComponent<PlayerMovement>();
+
+        look_Root = transform.GetChild(0);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprint();
+        Crouch();
+    }
+
+    void Sprint()
+    {
+
+        // if we have stamina we can sprint
+        //if (sprint_Value > 0f)
+        //{
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching)
+            {
+
+                playerMovement.speed = sprint_Speed;
+
+                //player_Footsteps.step_Distance = sprint_Step_Distance;
+                //player_Footsteps.volume_Min = sprint_Volume;
+                //player_Footsteps.volume_Max = sprint_Volume;
+
+            }
+
+        //}
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching)
+        {
+
+            playerMovement.speed = move_Speed;
+
+            //player_Footsteps.step_Distance = walk_Step_Distance;
+            //player_Footsteps.volume_Min = walk_Volume_Min;
+            //player_Footsteps.volume_Max = walk_Volume_Max;
+
+        }
+
+        //if (Input.GetKey(KeyCode.LeftShift) && !is_Crouching)
+        //{
+
+        //    sprint_Value -= sprint_Treshold * Time.deltaTime;
+
+        //    if (sprint_Value <= 0f)
+        //    {
+
+        //        sprint_Value = 0f;
+
+        //        // reset the speed and sound
+        //        playerMovement.speed = move_Speed;
+        //        player_Footsteps.step_Distance = walk_Step_Distance;
+        //        player_Footsteps.volume_Min = walk_Volume_Min;
+        //        player_Footsteps.volume_Max = walk_Volume_Max;
+
+
+        //    }
+
+        //    player_Stats.Display_StaminaStats(sprint_Value);
+
+        //}
+        //else
+        //{
+
+        //    if (sprint_Value != 100f)
+        //    {
+
+        //        sprint_Value += (sprint_Treshold / 2f) * Time.deltaTime;
+
+        //        player_Stats.Display_StaminaStats(sprint_Value);
+
+        //        if (sprint_Value > 100f)
+        //        {
+        //            sprint_Value = 100f;
+        //        }
+
+        //    }
+
+        //}
+
+
+    } // sprint
+
+    void Crouch()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            // if we are crouching - stand up
+            if (is_Crouching)
+            {
+
+                look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
+                playerMovement.speed = move_Speed;
+
+                //player_Footsteps.step_Distance = walk_Step_Distance;
+                //player_Footsteps.volume_Min = walk_Volume_Min;
+                //player_Footsteps.volume_Max = walk_Volume_Max;
+
+                is_Crouching = false;
+
+            }
+            else
+            {
+                // if we are not crouching - crouch
+
+                look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
+                playerMovement.speed = crouch_Speed;
+
+                //player_Footsteps.step_Distance = crouch_Step_Distance;
+                //player_Footsteps.volume_Min = crouch_Volume;
+                //player_Footsteps.volume_Max = crouch_Volume;
+
+                is_Crouching = true;
+
+            }
+
+        } // if we press c
+
+
+    } // crouch
+}
